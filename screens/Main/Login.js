ae56f05d0c7cebede3b072a51c96d8c734960a96
SafetyFlux <dekuwither@gmail.com>
2023-06-14 09:10:33 -0600

    Add sample login page

--- screens/Main/Login.js
+++ screens/Main/Login.js
@@ -1,11 +1,38 @@
-import { View, Text } from 'react-native';
+import { useContext } from 'react';
+import { View, Button, StyleSheet } from 'react-native';
+
+import { AuthContext } from '../../store/auth-context';
 
 function Login() {
+  // Set up context
+  const authCtx = useContext(AuthContext);
+
+  // Log in as user
+  function userLogin() {
+    authCtx.authenticate('xoxoxo', 'user');
+  }
+
   return (
-    <View>
-      <Text>This is the Login screen.</Text>
+    <View style={styles.container}>
+      <Button 
+        title='Sign in as user'
+        color='#003db6'
+        onPress={userLogin}
+      />
+      <Button
+        title='Sign in as administrator'
+        color='#770000'
+      />
     </View>
   );
 }
 
 export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
