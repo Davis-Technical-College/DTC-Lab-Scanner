edfbfe5a7c3590c83655c9fbf3d133061546a9a9
SafetyFlux <dekuwither@gmail.com>
2023-06-14 08:35:24 -0600

    Add user level to context

--- store/auth-context.js
+++ store/auth-context.js
@@ -4,6 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 // Create the base for the context
 export const AuthContext = createContext({
   token: '',
+  level: 0,
   isAuthenticated: false,
   authenticate: (token) => {},
   logout: () => {},
@@ -11,24 +12,30 @@ export const AuthContext = createContext({
 
 // Establish the provider for the context
 function AuthContextProvider({ children }) {
-  // Use a state for the auth token
+  // Use a state for the auth token and user level
   const [authToken, setAuthToken] = useState();
+  const [userLevel, setUserLevel] = useState();
 
-  // When logging in, set the token state
-  function authenticate(token) {
+  // When logging in, set the token and level states
+  function authenticate(token, level) {
     setAuthToken(token);
+    setUserLevel(level);
     AsyncStorage.setItem('token', token);
+    AsyncStorage.setItem('level', level);
   }
 
-  // When logging out, void the token state
+  // When logging out, void the token and level states
   function logout() {
     setAuthToken(null);
+    setUserLevel(0);
     AsyncStorage.removeItem('token');
+    AsyncStorage.removeItem('level');
   }
 
   // Set up value for context using the variables and functions created above
   const value = {
     token: authToken,
+    level: userLevel,
     isAuthenticated: !!authToken,
     authenticate: authenticate,
     logout: logout,
