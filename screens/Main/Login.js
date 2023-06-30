a17c76d190f37d2cc7dd8d4d891b96cd0fcc13c5
SafetyFlux <dekuwither@gmail.com>
2023-06-30 08:49:18 -0600

    Implement new package
    Use react-native-app-auth

--- screens/Main/Login.js
+++ screens/Main/Login.js
@@ -1,46 +1,33 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
-import { AzureInstance, AzureLoginView } from '@shedaltd/react-native-azure-ad-2';
+import { authorize } from 'react-native-app-auth';
 
 import { AuthContext } from '../../store/auth-context';
 
-const CREDENTIALS = {
-  client_id: '3bbf6b84-2d32-4341-a6d7-d24b0280bed8',
-  client_secret: 'dt68Q~6YsImCMuIQZ12iLgiLPDXabxYWwMF~Sbxc',
-  redirect_uri: 'https://login.microsoftonline.com/common/oauth2/nativeclient',
-  scope: 'User.Read',
+const config = {
+  issuer: 'https://login.microsoftonline.com/f9a4f5d0-300d-4aea-b514-f1c20d244fac',
+  clientId: '3bbf6b84-2d32-4341-a6d7-d24b0280bed8',
+  clientSecret: 'dt68Q~6YsImCMuIQZ12iLgiLPDXabxYWwMF~Sbxc',
+  redirectUrl: 'https://login.microsoftonline.com/common/oauth2/nativeclient',
+  scopes: ['User.Read'],
 };
 
 // Secret: dt68Q~6YsImCMuIQZ12iLgiLPDXabxYWwMF~Sbxc
 
-const Instance = new AzureInstance(CREDENTIALS);
-
 function Login () {
   const authCtx = useContext(AuthContext);
-  const [loggingIn, setLoggingIn] = useState(false);
 
-  function onLoginSuccess() {
-    Instance.getUserInfo().then(result => {
-      const username = `${result.givenName} ${result.surname}`;
-      const token = Instance.getToken().accessToken;
-      authCtx.authenticate(token, 'user', username);
-    }).catch(err => {
-      console.log(err);
-    });
-  }
-
-  function onLoginCancel() {
-    console.log('Cancelled');
+  async function azureLogin() {
+    try {
+      const result = await authorize(config);
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
-    loggingIn ? <AzureLoginView
-      azureInstance={Instance}
-      loadingMessage="Requesting access token"
-      onSuccess={onLoginSuccess}
-      onCancel={onLoginCancel}
-      onFailure={() => console.log('Login failed')}
-    /> : <View style={styles.container}>
+    <View style={styles.container}>
       <View style={styles.textContainer}>
         <Text style={styles.text}>
           This apps requires a Microsoft account with Davis Technical College.
@@ -50,7 +37,7 @@ function Login () {
       <View style={styles.buttonContainer}>
         <Button
           title='Login'
-          onPress={() => setLoggingIn(true)}
+          onPress={azureLogin}
         />
       </View>
     </View>
