5637469b6b5d4dab6077015a6c4676c82d1e63fd
SafetyFlux <dekuwither@gmail.com>
2023-07-26 08:25:48 -0600

    Update ResourceForm.js

--- components/Resources/ResourceForm.js
+++ components/Resources/ResourceForm.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Alert, ScrollView, Text, FlatList, StyleSheet } from 'react-native';
 
 import Input from '../UI/Input';
+import ListItem from './ListItem';
 import Button from '../UI/Button';
 
 function ResourceForm({ defaultValues, submitButtonLabel, onCancel, onSubmit }) {
@@ -34,6 +35,73 @@ function ResourceForm({ defaultValues, submitButtonLabel, onCancel, onSubmit }) 
       };
     });
   }
+
+  const formIsInvalid = !inputs.name.isValid || !inputs.description.isValid || 
+    !inputs.imageUri.isValid || !inputs.components.isValid;
+
+  // Functions for handling the component list
+  const addComponent = () => {
+    const newId = inputs.components.value.length;
+    const newComponent = { id: newId, text: '' };
+    const newData = { ...inputs.components.value, newComponent }
+
+    inputChangedHandler('components', newData);
+  }
+  const editComponent = (enteredText, position) => {
+    const filteredData = inputs.components.value.filter(item => item.id !== position);
+    const editedComponent = { id: position, text: enteredText };
+    const newData = { ...filteredData, editedComponent };
+
+    inputChangedHandler('components', newData);
+  }
+  const moveComponent = (direction) => {
+
+  }
+  const deleteComponent = () => {
+
+  }
+  const renderComponent = ({ item }) => {
+    return (
+      <ListItem
+        position={item.id} max={inputs.components.value.length} text={item.text}
+        onEdit={editComponent} onMove={moveComponent} onDelete={deleteComponent}
+      />
+    );
+  }
+
+  return (
+    <ScrollView style={styles.form}>
+      <Text style={styles.title}>Resource</Text>
+      <Input
+        label="Name"
+        multiline={false}
+        onUpdateValue={inputChangedHandler.bind(this, 'name')}
+        value={inputs.name.value}
+        isInvalid={!inputs.name.isValid}
+      />
+      <Input
+        label="Description"
+        multiline={true}
+        onUpdateValue={inputChangedHandler.bind(this, 'description')}
+        value={inputs.description.value}
+        isInvalid={!inputs.description.isValid}
+      />
+      <FlatList
+        data={inputs.components.value}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={renderComponent}
+        ItemSeparatorComponent={
+          <View style={styles.separator} />
+        }
+      />
+      <Button
+        color="#9000ff"
+        onPress={addComponent}
+      >
+        Add Component
+      </Button>
+    </ScrollView>
+  );
 }
 
 export default ResourceForm;
@@ -49,17 +117,15 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 24,
   },
-  inputsRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  rowInput: {
-    flex: 1,
-  },
   errorText: {
     margin: 8,
     textAlign: 'center',
     color: 'darkred',
+  },
+  separator: {
+    width: '90%',
+    height: 2,
+    backgroundColor: '#5b5b5b',
   },
   buttons: {
     flexDirection: 'row',
@@ -69,5 +135,5 @@ const styles = StyleSheet.create({
   button: {
     minWidth: 120,
     marginHorizontal: 8,
-  }
+  },
 });
