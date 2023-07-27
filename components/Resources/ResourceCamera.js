609d288e160085d54221a18b8ba366f165ad0f48
SafetyFlux <dekuwither@gmail.com>
2023-07-27 09:10:07 -0600

    Change camera permissions to useState

--- components/Resources/ResourceCamera.js
+++ components/Resources/ResourceCamera.js
@@ -1,30 +1,36 @@
+import { useState } from 'react';
 import { Modal, StyleSheet } from 'react-native';
 import { Camera, useCameraDevices } from 'react-native-vision-camera';
 
 import LoadingOverlay from '../UI/LoadingOverlay';
 
 function ResourceCamera({ visible, active }) {
+  // Create state for camera permissions
+  const [cameraPermission, setCameraPermission] = useState('');
+
   // Set up camera device
   const devices = useCameraDevices();
   const device = devices.back;
 
   // Check for permissions, get them if not available
   async function getCameraPermissions() {
-    const cameraPermission = await Camera.getCameraPermissionStatus();
+    const permission = await Camera.getCameraPermissionStatus();
 
-    if (cameraPermission !== 'authorized') {
-      const newCameraPermission = await Camera.requestCameraPermission();
-      return newCameraPermission;
+    if (permission !== 'authorized') {
+      const newPermission = await Camera.requestCameraPermission();
+      setCameraPermission(newPermission);
+      return;
     }
 
-    return cameraPermission;
+    setCameraPermission(permission);
+    return;
   }
-  const permissions = getCameraPermissions();
+  getCameraPermissions();
 
   // Return modal with camera
   return (
     <Modal visible={visible}>
-      {permissions == 'authorized' && device ? 
+      {cameraPermission == 'authorized' && device ? 
         <Camera
           style={StyleSheet.absoluteFill}
           device={device}
