22cb5fb6e3ac27efdcb54323339b367a0fcd41c8
SafetyFlux <dekuwither@gmail.com>
2023-07-28 08:45:01 -0600

    Add capture button

--- components/Resources/ResourceCamera.js
+++ components/Resources/ResourceCamera.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Modal, StyleSheet } from 'react-native';
+import { useState, useRef } from 'react';
+import { Modal, View, Pressable, StyleSheet } from 'react-native';
 import { Camera, useCameraDevices } from 'react-native-vision-camera';
 
 import LoadingOverlay from '../UI/LoadingOverlay';
@@ -11,6 +11,7 @@ function ResourceCamera({ visible, active }) {
   // Set up camera device
   const devices = useCameraDevices();
   const device = devices.back;
+  const camera = useRef(null);
 
   // Check for permissions, get them if not available
   async function getCameraPermissions() {
@@ -27,16 +28,33 @@ function ResourceCamera({ visible, active }) {
   }
   getCameraPermissions();
 
+  // Function for when the capture button is pressed
+  async function takePhotoHandler() {
+    if (camera.current !== null) {
+      const photo = await camera.current.takePhoto();
+      console.log(photo.path);
+    }
+  }
+
   // Return modal with camera
   return (
     <Modal visible={visible}>
       {cameraPermission == 'authorized' && device ? 
-        <Camera
-          style={StyleSheet.absoluteFill}
-          device={device}
-          isActive={active}
-          photo={true}
-        /> : 
+        <View style={[StyleSheet.absoluteFill, styles.cameraView]}>
+          <Camera
+            ref={camera}
+            style={StyleSheet.absoluteFill}
+            device={device}
+            isActive={active}
+            photo={true}
+          />
+          <View style={styles.captureContainer}>
+            <Pressable
+              style={({ pressed }) => [styles.captureButton, pressed && styles.capturePressed]}
+              onPress={takePhotoHandler}
+            />
+          </View>
+        </View> : 
         <LoadingOverlay />
       }
     </Modal>
