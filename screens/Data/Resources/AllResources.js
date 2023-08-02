43c3066c1fe06eb1b8a745e8057c03a31b67bd45
SafetyFlux <dekuwither@gmail.com>
2023-08-02 08:15:16 -0600

    Update AllResources.js

--- screens/Data/Resources/AllResources.js
+++ screens/Data/Resources/AllResources.js
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native';
+import { useContext } from 'react';
+
+import { ResourcesContext } from '../../../store/resources-context';
+import ResourcesOutput from '../../../components/Resources/ResourcesOutput';
 
 function AllResources() {
+  const resourcesCtx = useContext(ResourcesContext);
+
   return (
-    <View>
-      <Text>This is the AllResources screen.</Text>
-    </View>
+    <ResourcesOutput
+      resources={resourcesCtx.resources}
+      fallbackText="There are currently no resources. To add one, press '+'."
+    />
   );
 }
 
