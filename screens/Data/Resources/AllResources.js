abd9bbecc391f2be2c1b640151a886ba121277fb
SafetyFlux <dekuwither@gmail.com>
2023-08-02 08:19:43 -0600

    Update AllResources.js

--- screens/Data/Resources/AllResources.js
+++ screens/Data/Resources/AllResources.js
@@ -1,16 +1,13 @@
 import { useContext } from 'react';
 
 import { ResourcesContext } from '../../../store/resources-context';
-import ResourcesOutput from '../../../components/Resources/ResourcesOutput';
+import ResourceList from '../../../components/Resources/ResourceList';
 
 function AllResources() {
   const resourcesCtx = useContext(ResourcesContext);
 
   return (
-    <ResourcesOutput
-      resources={resourcesCtx.resources}
-      fallbackText="There are currently no resources. To add one, press '+'."
-    />
+    <ResourceList resources={resourcesCtx.resources} />
   );
 }
 
