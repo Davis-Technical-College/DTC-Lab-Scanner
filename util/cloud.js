f047b5508de670ba1309d39733a294779eca8d04
SafetyFlux <dekuwither@gmail.com>
2023-08-02 09:35:00 -0600

    Add function for retrieving download URL for image

--- util/cloud.js
+++ util/cloud.js
@@ -12,5 +12,18 @@ export async function uploadImage(imageUri) {
   await reference.putFile(imageUri);
 
   // Return the path to the file in the cloud
-  return `images/labkits/${filename}`;
+  const downloadUrl = await getDownloadUrl('labkits/' + filename);
+  return downloadUrl;
+}
+
+// Retrieve an image URL for display
+export async function getDownloadUrl(imageUri) {
+  let downloadUrl = '';
+  await storage().ref('/images/' + imageUri).getDownloadURL()
+  .then((url) => {
+    downloadUrl = url;
+  })
+  .catch((e) => console.log('Errors while downloading => ', e));
+
+  return downloadUrl;
 }
