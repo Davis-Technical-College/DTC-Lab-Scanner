5d567542a281d3a97ae8efb6f0cd5a4885df93ea
SafetyFlux <dekuwither@gmail.com>
2023-08-02 09:59:43 -0600

    Create basic resource details view

--- screens/Data/Resources/ResourceDetails.js
+++ screens/Data/Resources/ResourceDetails.js
@@ -1,11 +1,62 @@
-import { View, Text } from 'react-native';
+import { useEffect } from 'react';
+import { ScrollView, Image, View, Text, StyleSheet } from 'react-native';
 
-function ResourceDetails() {
+function ResourceDetails({ route, navigation }) {
+  // Get route params
+  const resource = route.params.resource;
+
+  // Set title based on resource name
+  useEffect(() => {
+    navigation.setOptions({
+      title: resource.name,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>This is the ResourceDetails screen.</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={{ uri: resource.imageUri }} />
+      </View>
+
+      <View style={styles.detailsContainer}>
+        <Text style={styles.details}>
+          <Text style={styles.detailsBold}>Description: </Text>
+          {resource.description}
+        </Text>
+        <Text style={styles.details}>
+          <Text style={styles.detailsBold}>Current User: </Text>
+          {!!resource.currentUser ? resource.currentUser : 'None'}
+        </Text>
+      </View>
+    </ScrollView>
   );
 }
 
 export default ResourceDetails;
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 50,
+  },
+  image: {
+    width: '80%',
+    height: '80%',
+    minHeight: 330,
+  },
+  detailsContainer: {
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  details: {
+    marginHorizontal: 30,
+    color: '#000000',
+    textAlign: 'left',
+    fontSize: 18,
+  },
+  detailsBold: {
+    fontWeight: 'bold',
+  },
+});
